826a14b99644bbcd77b69386339b4d7241087bdc
Nico ojeda <nico_ojeda86@hotmail.com>
2023-02-15 19:50:22 -0300

    se define el modelo estimate

---
+++ models/Estimate.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+        image:{type: String, required: true}, 
+        name:{type: String, required: true}, 
+        date:{type: Date, required: true},
+        description:{type: String, required: true},
+        category:{type: String, required: true},
+        place:{type: String, required: true}, 
+        capacity:{type: Number, required: true}, 
+        estimate:{type: Number, required: true},
+        price:{type: Number, required: true}
+})
+
+const Estimate = mongoose.model('events', schema)
+
+module.exports = Estimate
