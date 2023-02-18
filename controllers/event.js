0960118c76f4d5f1f9d51eb4a5142444e37ae5b3
Nico ojeda <nico_ojeda86@hotmail.com>
2023-02-18 00:28:14 -0300

    se define controlador para leer eventos

--- controllers/event.js
+++ controllers/event.js
@@ -3,10 +3,20 @@ const Event = require('../models/Event')
 const controller = {
 
 read: async(req,res) =>{
-    try{
 
-    } catch{
-        
+    try{
+        let todos = await Event.find()
+        res.status(200).json({
+            response: todos,
+            success: true,
+            message: "se encuentran eventos"
+        })
+
+    } catch(error){
+        res.status(400).json({
+            success:false,
+            message: error.message
+        })
     }
 }
 
